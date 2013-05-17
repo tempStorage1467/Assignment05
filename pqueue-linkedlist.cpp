71edbfd8f726f90d570a7848a2212a79fb2f088c
Eric Beach <ebeach@google.com>
2013-05-17 12:08:26 -0400

    working second part

--- pqueue-linkedlist.cpp
+++ pqueue-linkedlist.cpp
@@ -40,42 +40,21 @@ void LinkedListPriorityQueue::enqueue(string value) {
         temp->next = list;
         list = temp;
     } else {
-        /*
-         hello
-         zoo
-         tom
-         abc
-         inside
-         */
         Cell* curr = list;
         Cell* last;
         do {
             last = curr;
-            curr = list->next;
-        } while (curr != NULL && newCell->value >curr->value);
-        
-        /*
-        for (curr = list;
-             newCell->value > curr->value && curr->next != NULL;
-             curr = curr->next) {
-            
-        }
-         */
-            /*
-        while (newCell->value > curr->value) {
-            cout << newCell->value << " is larger than " << curr->value << endl;
-            if (curr->next == NULL) {
-                break;
-            } else {
-                curr = curr->next;
+           // cout << "Last: " << last->value << endl;
+            curr = curr->next;
+            if (curr != NULL) {
+          //      cout << "Current: " << curr->value << endl;
             }
-        }
-             */
+        } while (curr != NULL && newCell->value >curr->value);
+
         newCell->next = last->next;
         last->next = newCell;
     }
     listSize++;
-    cout << listSize << endl;
     printLinkedList();
 }
 
@@ -85,28 +64,29 @@ string LinkedListPriorityQueue::peek() {
 }
 
 string LinkedListPriorityQueue::dequeueMin() {
-    printLinkedList();
 	if (isEmpty()) error("Cannot peek at an empty list");
+    printLinkedList();
     Cell* currentSmallest = list;
     string val = currentSmallest->value;
     Cell* nextSmallest = list->next;
-    if (nextSmallest->next != NULL) {
-        // we have more than one element in the list, so move
-        //   the pointer to the first (and smallest) element left
-        //   in the list after we remove the current one
-        list = nextSmallest;
-    }
+    list = nextSmallest;
 
+    // delete the allocated memory to prevent a leak
+    delete currentSmallest;
     listSize--;
-    delete currentSmallest; // delete the allocated memory to prevent a leak
+    printLinkedList();
 	return val;
 }
 
 void LinkedListPriorityQueue::printLinkedList() {
-    cout << "going to print linked list" << endl;
+    return;
+    cout << "going to print linked list of size " << listSize << endl;
     for (Cell* current = list;
          current != NULL;
          current = current->next) {
-        cout << "printing: " << current->value << endl;
+        if (current != NULL) {
+            cout << "printing: " << current->value << endl;
+        }
     }
+    cout << "finished printing linked list of size " << listSize << endl;
 }
